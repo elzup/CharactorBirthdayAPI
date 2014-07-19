02914dc678eb5b719413a2694df7af9e82ff03d1
elzzup <guild0105@gmail.com>
2014-07-19 19:05:26 +0900

    udpate BirthdayDBMaster class

--- db_creater.php
+++ db_creater.php
@@ -5,7 +5,7 @@ require_once './keys.php';
 
 $dsn = 'pgsql:dbname=' . DB_NAME .' host=' . DB_HOST . ' port=' . DB_PORT;
 $user = DB_USER;
-$password = DB_PASSWORD;
+$password = DB_PASS;
 
 try {
     $bm = new BirthdayDBManager(new PDO($dsn, $user, $password));
@@ -22,16 +22,18 @@ class BirthdayDBManager {
         $this->dbh = $dbh;
     }
 
-    public function insert_charactors($charactors) {
+    public function regist_charactors(array $charactors) {
+        foreach ($charactors as $c) {
+            $this->regist_charactor($c);
+        }
     }
 
-    public function insert_charactor($c, $title_id) {
-        $stmt = $this->dbh->prepare('INSERT INTO charactor (charactor_id, charactor_name, birthday_m, birthday_d) VALUES (:ID, :NAME, :BM, :BD)');
-        $stmt->bindValue(':ID', $c->get_id());
-        $stmt->bindValue(':NAME', $c->get_name());
-        $stmt->bindValue(':BM', $c->get_date_m());
-        $stmt->bindValue(':BD', $c->get_date_d());
-        $stmt->execute();
+    public function regist_charactor($c) {
+        $title_id = $this->regist_tilte($c->get_title());
+        if ($charactor_id = $this->select_charactor_id($c->get_name())) {
+            return $charactor_id;
+        }
+        return $this->insert_charactor($c, $title_id);
     }
 
     public function regist_title($title_name) {
@@ -42,7 +44,26 @@ class BirthdayDBManager {
         return $id;
     }
 
-    public function select_title_id($title_name) {
+    private function insert_charactor($c, $title_id) {
+        $stmt = $this->dbh->prepare('INSERT INTO charactor (charactor_name, birthday_m, birthday_d) VALUES (:NAME, :BM, :BD)');
+        $stmt->bindValue(':NAME', $c->get_name());
+        $stmt->bindValue(':BM', $c->get_date_m());
+        $stmt->bindValue(':BD', $c->get_date_d());
+        $stmt->execute();
+        return $this->select_charactor_id($c->get_name());
+    }
+
+    public function select_charactor_id($charactor_name) {
+        $stmt = $this->dbh->prepare('SELECT charactor_id from charactor where charactor_name = :NAME');
+        $stmt->bindValue(':NAME', $charactor_name);
+        $stmt->execute();
+        if ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            return $row['charactor_id'];
+        }
+        return -1;
+    }
+
+    private function select_title_id($title_name) {
         $stmt = $this->dbh->prepare('SELECT title_id from title where title_name = :NAME');
         $stmt->bindValue(':NAME', $title_name);
         $stmt->execute();
@@ -52,7 +73,7 @@ class BirthdayDBManager {
         return -1;
     }
 
-    public function insert_title($title_name) {
+    private function insert_title($title_name) {
         try {
             $stmt = $this->dbh->prepare("INSERT INTO title (title_name) VALUES (:NAME);");
             $stmt->bindValue(':NAME', $title_name);
