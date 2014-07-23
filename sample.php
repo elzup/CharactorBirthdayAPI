bea96f8fd1c89834e0b57eedf3352a4b5b927649
elzzup <guild0105@gmail.com>
2014-07-23 19:06:24 +0900

    create charactor search api

--- sample.php
+++ sample.php
@@ -4,7 +4,6 @@ $url_top = 'http://localhost/birthday/';
 
 /*
  * titles/search 作品名検索
- *
  */
 $parameter = array(
     'q' => 'ゆるゆり',
@@ -23,4 +22,19 @@ if (isset($tilte->charactors)) {
         echo "{$c->name}({$c->day_m}月{$c->day_d}日)" . PHP_EOL;
     }
 }
+exit;
+
+/*
+ * charactors/search キャラ名検索
+ */
+$parameter = array(
+    'q' => 'ひたぎ',
+    'include_details' => true,
+);
+$url_main = 'charactors/search';
+$url_tail = '?' . http_build_query($parameter);
+$url = $url_top . $url_main . $url_tail;
+
+$json = file_get_contents($url);
+$res = json_decode($json);
 
